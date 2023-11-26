bc745354f0e5d1c8ff07fe0cbff29967f0caab07
moelkady27 <mohamedkhaledelkady11@gmail.com>
2023-11-26 20:37:00 +0200

    done favorites

---
+++ app/src/main/java/com/example/foodapp/adapter/FavoriteMealsAdapter.kt
@@ -0,0 +1,62 @@
+package com.example.foodapp.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.foodapp.R
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.pojo.MealsCategory
+import kotlinx.android.synthetic.main.meal_card.view.img_meal1
+import kotlinx.android.synthetic.main.meal_card.view.tv_meal_name
+
+class FavoriteMealsAdapter : RecyclerView.Adapter<FavoriteMealsAdapter.MyViewHolder>() {
+
+    private var list : List<Meal> = ArrayList()
+
+    fun setFavoriteMealsList(favoriteMeals: List<Meal>) {
+        this.list = favoriteMeals
+        notifyDataSetChanged()
+    }
+
+//    private val diffUtil = object : DiffUtil.ItemCallback<Meal>(){
+//        override fun areItemsTheSame(oldItem: Meal, newItem: Meal): Boolean {
+//            return oldItem.idMeal == newItem.idMeal
+//        }
+//
+//        override fun areContentsTheSame(oldItem: Meal, newItem: Meal): Boolean {
+//            return oldItem == newItem
+//        }
+//
+//    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.meal_card ,
+            parent ,
+            false
+        )
+        return FavoriteMealsAdapter.MyViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val x = list[position]
+
+        Glide.with(holder.itemView)
+            .load(x.strMealThumb)
+            .into(holder.itemView.img_meal1)
+
+        holder.itemView.tv_meal_name.text = x.strMeal
+    }
+
+
+    class MyViewHolder(view : View) : RecyclerView.ViewHolder(view)
+
+}
--- app/src/main/java/com/example/foodapp/fragments/FavoriteMealsFragment.kt
+++ app/src/main/java/com/example/foodapp/fragments/FavoriteMealsFragment.kt
@@ -1,13 +1,28 @@
 package com.example.foodapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foodapp.R
+import com.example.foodapp.activities.MainActivity
+import com.example.foodapp.adapter.FavoriteMealsAdapter
+import com.example.foodapp.adapter.MostPopularAdapter
+import com.example.foodapp.pojo.Meal
+import com.example.foodapp.viewModel.HomeViewModel
+import kotlinx.android.synthetic.main.fragment_favorite_meals.fav_rec_view
+import kotlinx.android.synthetic.main.fragment_home.rec_view_meals_popular
 
 class FavoriteMealsFragment : Fragment() {
+
+    private lateinit var favoriteMvvm : HomeViewModel
+
+    private lateinit var adapter: FavoriteMealsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,8 +36,29 @@ class FavoriteMealsFragment : Fragment() {
         )
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        favoriteMvvm = (activity as MainActivity).viewModel
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        observeFavorites()
+        showRecycleView()
+    }
+
+    private fun observeFavorites() {
+        favoriteMvvm.observeFavoriteMealsLiveData().observe(viewLifecycleOwner , Observer {  meals ->
+            adapter.setFavoriteMealsList(favoriteMeals = meals as ArrayList<Meal>)
+        })
+    }
+
+    private fun showRecycleView() {
+        adapter = FavoriteMealsAdapter()
+        fav_rec_view.apply {
+            layoutManager = GridLayoutManager(context, 2 , GridLayoutManager.VERTICAL, false)
+            adapter = this@FavoriteMealsFragment.adapter
+        }
+    }
 }
